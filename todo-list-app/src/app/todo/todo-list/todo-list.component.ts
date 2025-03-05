02012bf647c8a27a34be238687a2e9971c050c7f
Frédéric GAURAT <fgaurat@gmail.com>
2025-03-05 13:59:22 +0100

    add ul li

--- todo-list-app/src/app/todo/todo-list/todo-list.component.ts
+++ todo-list-app/src/app/todo/todo-list/todo-list.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { Todos } from '../../models/todo';
+import { EMPTY, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+
+
+  private todoService = inject(TodoService);
+  public todos$:Observable<Todos>=EMPTY;
+
+  ngOnInit(): void {
+
+    this.todos$=this.todoService.findAll();
+
+
+  }
+
+
 
 }
