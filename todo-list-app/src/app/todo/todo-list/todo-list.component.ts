3b498ef12ebeadbf8292c6b961598ca40b977e6f
Frédéric GAURAT <fgaurat@gmail.com>
2025-03-05 14:11:47 +0100

    add mattable

--- todo-list-app/src/app/todo/todo-list/todo-list.component.ts
+++ todo-list-app/src/app/todo/todo-list/todo-list.component.ts
@@ -3,10 +3,10 @@ import { TodoService } from '../../services/todo.service';
 import { Todos } from '../../models/todo';
 import { EMPTY, Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-
+import {MatTableModule} from '@angular/material/table';
 @Component({
   selector: 'app-todo-list',
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
@@ -15,6 +15,8 @@ export class TodoListComponent implements OnInit {
 
   private todoService = inject(TodoService);
   public todos$:Observable<Todos>=EMPTY;
+  // displayedColumns: string[] = ['id', 'title', 'completed'];
+  displayedColumns: string[] = ['id'];
 
   ngOnInit(): void {
 
